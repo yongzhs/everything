a8cdb27955578825fa3262fcbaa80eb75b4bc3a7
Yongzhi Shao <yongzhs@clemson.edu>
2020-06-07 18:06:48 -0400

    Update plot_sine_theta.py

--- plot_sine_theta.py
+++ plot_sine_theta.py
@@ -1,29 +1,15 @@
-# exercise of polar cordinate plot...
-# https://matplotlib.org/3.1.0/gallery/mplot3d/surface3d.html
-# https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html
-
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import mpl_toolkits.mplot3d.axes3d as axes3d
 
-n_theta = 181
-step_theta = 180/(n_theta - 1)
-n_phi = 361
-step_phi = 360/(n_phi - 1)
-x = np.zeros([n_theta, n_phi], dtype = float)
-y = np.zeros([n_theta, n_phi], dtype = float)
-z = np.zeros([n_theta, n_phi], dtype = float)
-for i in range(n_theta):
-    for j in range(n_phi):
-        x[i][j] = math.sin(i * step_theta * math.pi/180) * math.sin(i * step_theta * math.pi/180) * math.cos(j * step_phi * math.pi/180)
-        y[i][j] = math.sin(i * step_theta * math.pi/180) * math.sin(i * step_theta * math.pi/180) * math.sin(j * step_phi * math.pi/180)
-        z[i][j] = math.sin(i * step_theta * math.pi/180) * math.cos(i * step_theta * math.pi/180)
-        
+theta, phi = np.linspace(0, 2 * np.pi, 40), np.linspace(0, np.pi, 40)
+THETA, PHI = np.meshgrid(theta, phi)
+R = np.sin(PHI)
+X = R * np.sin(PHI) * np.cos(THETA)
+Y = R * np.sin(PHI) * np.sin(THETA)
+Z = R * np.cos(PHI)
 fig = plt.figure()
-ax = fig.gca(projection = '3d')
-surf = ax.plot_surface(x, y, z, cmap = plt.cm.coolwarm)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-fig.colorbar(surf) # , shrink = 0.5, aspect = 1
+ax = fig.add_subplot(1,1,1, projection='3d')
+plot = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('jet'), linewidth=0, antialiased=False, alpha=0.5)
+fig.colorbar(plot)
 plt.show()
